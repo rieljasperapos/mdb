7199893185c6333218d78150f913a8f295ed6243
Riel <jasper.apos@gmail.com>
2023-12-26 23:36:07 +0800

    refactor: env port

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import { IBook } from './schema/bookSchema';
 import { config } from 'dotenv';
 
 config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 
