29c6cad9dc9b7d55988e4307c0fff5e9554a02b3
Riel <jasper.apos@gmail.com>
2023-12-30 10:52:56 +0800

    style: button and books table

--- client/src/app/books/page.tsx
+++ client/src/app/books/page.tsx
@@ -1,6 +1,17 @@
 "use client"
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
 
 interface Books {
     title: string;
@@ -36,13 +47,40 @@ const Books = () => {
     return (
         <div className="flex flex-col gap-4 justify-center items-center h-screen">
             <div className="mb-10">
-                <Link href='/' className="p-2 bg-gray-200 rounded-lg hover:bg-gray-100">Back</Link>
+                <Button asChild>
+                    <Link href='/'>Back</Link>
+                </Button>
             </div>
-            <div className="grid gap-5">
+            <div className="grid gap-10">
                 <h1 className="mb-2">HELLO THIS IS BOOKS PAGE</h1>
-                {books?.map((book, idx) => (
+                <Table>
+                    <TableCaption>A list of books.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[100px]">Title</TableHead>
+                            <TableHead>Author</TableHead>
+                            <TableHead>Year Published</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                            {books?.map((book, idx) => (
+                                <TableRow key={idx}>
+                                    <TableCell className="font-medium">{book.title}</TableCell>
+                                    <TableCell>{book.author}</TableCell>
+                                    <TableCell>{book.publishYear}</TableCell>
+                                    <TableCell>
+                                        <Button variant="outline" asChild>
+                                            <Link href={`/books/${book.title}`}>View</Link>
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                    </TableBody>
+                </Table>
+
+                {/* {books?.map((book, idx) => (
                     <Link href={`/books/${book.title}`} key={idx} className="font-medium">{book.title}</Link>
-                ))}
+                ))} */}
             </div>
         </div>
     )
