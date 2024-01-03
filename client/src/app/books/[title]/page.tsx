4cb41344479fb99fa13901bcc9637fb8ac41c9fe
Riel <jasper.apos@gmail.com>
2024-01-04 02:14:08 +0800

    chore: display book image

--- client/src/app/books/[title]/page.tsx
+++ client/src/app/books/[title]/page.tsx
@@ -3,10 +3,11 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
-interface Book {
+interface IBook {
     title: string;
     author: string;
     publishYear: number;
+    [key: string]: any;
 };
 
 interface BookParams {
@@ -16,7 +17,7 @@ interface BookParams {
 };
 
 const Book: React.FC<BookParams> = ({params : { title }}) => {
-    const [bookData, setBookData] = useState<Book | null>(null);
+    const [bookData, setBookData] = useState<IBook | null>(null);
     
     useEffect(() => {
         fetch(`http://localhost:3001/books/${title}`, {
@@ -33,7 +34,7 @@ const Book: React.FC<BookParams> = ({params : { title }}) => {
 
             return res.json();
         })
-        .then((data: Book) => {
+        .then((data: IBook) => {
             if (data) {
                 console.log(data);
                 setBookData(data);
@@ -43,20 +44,36 @@ const Book: React.FC<BookParams> = ({params : { title }}) => {
             console.error(err);
         })
     }, [])
+    
     return (
-        <div className="flex flex-col gap-5 justify-center items-center h-screen">
+        <div className="flex flex-col gap-5 justify-center items-center p-20">
             <div className="mb-10">
                 <Button asChild>
                     <Link href='/books'>Back</Link>
                 </Button>
             </div>
-            <div className="grid gap-4">
+            <div className="grid gap-4 px-40">
+                <div className="flex justify-center items-center">
+                    {bookData?.image ? (
+                        <img src={bookData.image} alt={bookData.title} style={{ maxWidth: '100%', maxHeight: '400px' }} className="mb-16" />
+                    ) : (
+                        <p>No image available</p>
+                    )}
+                </div>
                 <h1>Title:</h1>
                 <p>{bookData?.title}</p>
                 <h1>Author:</h1>
                 <p>{bookData?.author}</p>
                 <h1>Year Published:</h1>
                 <p>{bookData?.publishYear}</p>
+                {bookData?.description ? 
+                    <>
+                        <h1>Description:</h1>
+                        <p>{bookData?.description}</p>
+                    </> 
+                    : 
+                    ""
+                }
             </div>
         </div>
     )
