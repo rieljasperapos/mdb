49866a5819ccf7270f22a4439daa05f9b2399304
Riel <jasper.apos@gmail.com>
2024-01-09 14:13:51 +0800

    feat: zod register schema for validations

--- client/src/schemas/index.ts
+++ client/src/schemas/index.ts
@@ -8,3 +8,18 @@ export const LoginSchema = z.object({
     message: "Password is required"
   })
 });
+
+export const RegisterSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters"
+  }),
+  confirmPassword: z.string().min(8, {
+    message: "Password must be at least 8 characters"
+  })
+})
+.refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords don't match",
+  path: ["confirm"],
+});
