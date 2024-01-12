a5cd758f8d20de25841a09fb45802601444bc639
Riel <jasper.apos@gmail.com>
2024-01-12 21:10:04 +0800

    refactor: changed format

--- client/src/components/auth/show-social.tsx
+++ client/src/components/auth/show-social.tsx
@@ -1,6 +1,5 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
-
 import { Button } from "@components/ui/button";
 
 export const Socials = () => {
@@ -11,11 +10,23 @@ export const Socials = () => {
           <h1>Login With</h1>
         </div>
         <div className="flex gap-x-4 w-full">
-          <Button size="lg" className="w-full" variant="outline">
-            <FcGoogle className="h-5 w-5" />
+          <Button 
+            size="lg" 
+            className="w-full" 
+            variant="outline"
+          >
+            <FcGoogle 
+              className="h-5 w-5" 
+            />
           </Button>
-          <Button size="lg" className="w-full" variant="outline">
-            <FaGithub className="h-5 w-5" />
+          <Button 
+            size="lg" 
+            className="w-full" 
+            variant="outline"
+          >
+            <FaGithub 
+              className="h-5 w-5" 
+            />
           </Button>
         </div>
       </div>
