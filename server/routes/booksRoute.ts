7278bc45fff5796cdedfbab81e7bba97d6d14515
Riel <jasper.apos@gmail.com>
2024-01-13 15:30:12 +0800

    feat[server]: protected endpoints only accessible if user is authenticated

--- server/routes/booksRoute.ts
+++ server/routes/booksRoute.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import * as booksController from '../controllers/booksController'
+import * as userController from '../controllers/userController'
 import multer from 'multer';
 
 const router = express.Router();
@@ -8,9 +9,9 @@ const upload = multer({
   storage: storage,
 });
 
-router.get('/books', booksController.getAllBook);
+router.get('/books', userController.authenticate, booksController.getAllBook);
 router.post('/books/:title', booksController.searchBookByTitle);
-router.post('/add-book', upload.single('image'), booksController.addBook);
+router.post('/add-book', upload.single('image'), userController.authenticate, booksController.addBook);
 router.put('/update-book/:title', booksController.updateBook);
 router.delete('/delete-book/:title', booksController.deleteBook);
 router.get('/book-count', booksController.addedBooksByMonth);
--- server/routes/userRoute.ts
+++ server/routes/userRoute.ts
@@ -3,7 +3,8 @@ import * as userControllers from '../controllers/userController';
 
 const router = express.Router();
 
-router.get("/users", userControllers.getUsers);
-router.post("/add-user", userControllers.addUser);
+router.get("/users", userControllers.authenticate, userControllers.getUsers);
+router.post("/register-user", userControllers.registerUser);
+router.post("/login-user", userControllers.loginUser);
 
 export = router;
