782114847a1190442005a299d8157f427b669fcd
Riel <jasper.apos@gmail.com>
2024-01-13 15:26:42 +0800

    style: gap-10

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <>
       <header className="sticky flex justify-center p-10">
-        <div className="flex items-center justify-between w-full h-16 max-w-3xl px-4 mx-auto sm:px-6">
+        <div className="flex items-center justify-between gap-10 w-full h-16 max-w-3xl px-4 mx-auto sm:px-6">
           <MainNav />
           <UserButton />
         </div>
