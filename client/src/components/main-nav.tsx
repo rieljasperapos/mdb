42c44478c453a022a636f33c76289f5f6b7e1812
Riel <jasper.apos@gmail.com>
2024-01-13 15:27:02 +0800

    refactor: change the route to /dashboard

--- client/src/components/main-nav.tsx
+++ client/src/components/main-nav.tsx
@@ -4,7 +4,7 @@ export const MainNav = () => {
   return (
     <>
       <ul className='flex gap-4'>
-        <Link href='/' className='cursor-pointer hover:text-orange-500 hover:underline'>Dashboard</Link>
+        <Link href='/dashboard' className='cursor-pointer hover:text-orange-500 hover:underline'>Dashboard</Link>
         <Link href='/books' className='cursor-pointer hover:text-orange-500 hover:underline'>Books</Link>
         <Link href='/search' className='cursor-pointer hover:text-orange-500 hover:underline'>Search</Link>
         <Link href='/add' className='cursor-pointer hover:text-orange-500 hover:underline'>Add</Link>
