028a886e87429ac55c28584c6cee388440ef4f00
Riel <jasper.apos@gmail.com>
2024-01-13 15:29:49 +0800

    feat[server]: implemented login logic

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,9 +1,18 @@
 import { IUser } from "../schema/userSchema";
 import { User } from "../models/userModel";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import bcrypt from 'bcryptjs'
+import jwt, { VerifyErrors } from 'jsonwebtoken'
+import { config } from 'dotenv';
+
+interface CustomRequest extends Request {
+  user?: any
+};
+
+config();
 
 export const getUsers = (req: Request, res: Response) => {
+  console.log(req);
   User.find({})
     .then((data: IUser[]) => {
       res.send(data);
@@ -14,7 +23,53 @@ export const getUsers = (req: Request, res: Response) => {
     })
 }
 
-export const addUser = (req: Request, res: Response) => {
+export const loginUser = (req: Request, res: Response) => {
+  // TODO: Compare encrypted password, JWT Sign
+  if (!req.body.email || !req.body.password) {
+    return res.send({
+      message: "Please fill out all the missing fields",
+      status: false
+    });
+  }
+
+  User.findOne({
+    email: req.body.email
+  })
+  .then((data: IUser | null) => {
+    if (data) {
+      console.log("DATA: ", data.password);
+      bcrypt.compare(req.body.password, data.password as string, (err, isPasswordMatched) => {
+        if (isPasswordMatched) {
+          const user = {
+            name: data.name,
+            email: data.email
+          }
+          // TO DO: JWT Sign
+          const token = jwt.sign({ user }, process.env.SECRET_KEY as string, {expiresIn: '1h'});
+          console.log(token);
+          res.cookie('accessToken', token, {maxAge: 3600000, httpOnly: true})
+          res.send({
+            message: "Successfully logged in",
+            status: true
+          });
+        }
+      })
+    } else {
+      res.send({
+        message: "Account not found",
+        status: false
+      });
+    }
+  })
+  .catch((err: Error) => {
+    res.send({
+      message: `Error ${err}`,
+      status: false
+    });
+  })
+}
+
+export const registerUser = (req: Request, res: Response) => {
   // TODO: Hash password, validations
   bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
     if (err) {
@@ -64,4 +119,28 @@ export const addUser = (req: Request, res: Response) => {
       }
     })
   });
+}
+
+export const authenticate = (req: CustomRequest, res: Response, next: NextFunction) => {
+  console.log("AUTHENTICATE COOKIE: ", req.cookies.accessToken);
+  console.log(req.cookies);
+  const authToken = req.cookies.accessToken;
+
+  if (!authToken) {
+    res.send({
+      message: "Unauthorized Access"
+    });
+  } else {
+    jwt.verify(authToken, process.env.SECRET_KEY as string, (err: VerifyErrors | null, data: any) => {
+      if (err) {
+        res.send({
+          message: "Invalid Token",
+          status: false
+        });
+      } else {
+        req.user = data;
+        next();
+      }
+    })
+  }
 }
