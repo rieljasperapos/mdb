63a565ed25ad2f4cf8532c52b8532301d368b754
Riel <jasper.apos@gmail.com>
2024-01-13 15:24:57 +0800

    refactor: moved header to dashboard

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div>
-          <Header />
           {children}
           <Toaster />
         </div>
