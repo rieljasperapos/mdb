5fc7ae2be10d1fca66794df4b274fe3180dd69e1
Riel <jasper.apos@gmail.com>
2024-01-16 22:02:42 +0800

    feat: authenticate using nextauth

--- server/routes/books.routes.ts
+++ server/routes/books.routes.ts
@@ -9,9 +9,9 @@ const upload = multer({
   storage: storage,
 });
 
-router.get('/books', authenticate, booksController.getAllBook);
+router.get('/books', booksController.getAllBook);
 router.post('/books/:title', booksController.searchBookByTitle);
-router.post('/add-book', upload.single('image'), authenticate, booksController.addBook);
+router.post('/add-book', upload.single('image'), booksController.addBook);
 router.put('/update-book/:title', booksController.updateBook);
 router.delete('/delete-book/:title', booksController.deleteBook);
 router.get('/book-count', booksController.addedBooksByMonth);
