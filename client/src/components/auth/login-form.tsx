a2caa790a7d1589b7b76334b8c0c41bf762cd910
Riel <jasper.apos@gmail.com>
2024-01-21 09:50:04 +0800

    removed extra spaces

--- client/src/components/auth/login-form.tsx
+++ client/src/components/auth/login-form.tsx
@@ -3,7 +3,6 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 import { LoginSchema } from "@/schemas"
-
 import {
   Form,
   FormControl,
