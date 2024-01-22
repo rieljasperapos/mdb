4fd6d4c8ab281fec90341040903ddc9d21f02ba0
Riel <jasper.apos@gmail.com>
2024-01-22 18:04:11 +0800

    refactor: custom path

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { UserButton } from '@/components/user-button'
+import { UserButton } from '@components/user-button'
 import { getServerSession } from 'next-auth';
 
 export default async function Home() {
